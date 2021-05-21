c68d1c80eea45082bfd9d4cd2542ebb9421875ac
zhou-l <zhou.liang.86.mail@gmail.com>
2021-05-21 16:08:49 +0800

    Added body parts images

--- rnaViewer.js
+++ rnaViewer.js
@@ -12,9 +12,14 @@ var g_dpheight = 600;
 // body view
 var g_bvwidth = 400;
 var g_bvheight= 600;
+var g_orgBodyImgWidth = 493;
+var g_orgBodyImgHeight = 1125;
 
 // global data
 var g_tmpMeanVal = [];
+var g_exprValColorMap = [];
+var g_pathFiles = ['./data/pathHeadNeck.txt','./data/pathLegs.txt','./data/pathTorso.txt',
+'./data/pathPerinaeum.txt','./data/pathArmsHands.txt'];
 
 var g_sgwidth = 1400;
 var g_sgLegendWidth = 100;
@@ -36,10 +41,143 @@ function drawBodyView(data, className, divName, width, height, margin)
                     .attr('x','0')
                     .attr('y','0');
     var logoUrl = "./data/body.png";
-    svg_img .attr('height', height)
-    .attr('width',width)
-    .attr('xlink:href', logoUrl);
 
+
+    var parts = [1, 2, 3, 4];
+    var nodeParts = svg.selectAll(".bodyParts")
+    .data(parts)
+    .enter()
+    .append("circle")
+    .attr("class", "bodyParts")
+    .attr("id",function(d,i){return i;})
+    .attr("r",10)
+    .attr("cx", function(d,i){return 20 + 25*i;})
+    .attr("cy", function(d,i){return 20 + 25*i;})
+    .style("fill", function(d,i){return g_exprValColorMap(i/parts.length)})
+    ;
+
+    // Get image outline
+    // var getImageOutline = require(['image-outline']);
+    // getImageOutline(logoUrl, function (err, polygon) {
+    //     if (err) {
+    //         // err is an Error; handle it
+    //         return;
+    //     }
+
+    //     // polygon is now an array of {x,y} objects. Have fun!
+    //     console.log(polygon);
+    // });
+
+    // Draw parts
+    var parts = [1];//[1,2,3,4,5];
+    var nodePartSilhouette = svg.append("g").attr("class", "bodyShape");
+    // nodePartSilhouette.select(".bodyOutline")
+    // .data(g_pathFiles)
+    // .enter()
+    // .append('path')
+    // .attr("class","bodyOutline")
+    // .attr("id", function(d,i){return "bodyPart"+i;});
+
+    // .select(".bodyOutline");
+    // .data(parts)
+    // .enter();
+    var allPathData = [];
+    var xscale = 0.65*width/g_orgBodyImgWidth;
+    var yscale = height/g_orgBodyImgHeight;
+    d3.queue()
+    .defer(d3.csv, "./data/pathPalmSole.txt")
+    .defer(d3.csv, "./data/pathExtremities.txt")
+    .await(function(error, data1, data2) {
+    if (error) throw error;
+        var bodyPart1 = nodePartSilhouette
+        .append('path')
+        .attr("id","bodyPart1")
+        .attr("class","bodyOutline")
+        .attr("d", function(){
+            var pathStr = "M ";
+
+            for(var i = 0; i < data1.length; i++)
+            {
+                if(i > 0)
+                    pathStr += "L " + data1[i].x * xscale + " " + data1[i].y * yscale+" ";
+                else
+                    pathStr += data1[i].x * xscale + " " + data1[i].y * yscale +" ";
+                if(i == data1.length-1)
+                    pathStr += "z";
+            }
+            return pathStr;
+        })
+        .style("fill", function(){return g_exprValColorMap(0.2);})
+        .style("opacity","0.5")
+         .attr("transform","translate("+ 70 +","+0+")");
+
+         var bodyPart2 = nodePartSilhouette
+         .append('path')
+         .attr("id","bodyPart2")
+         .attr("class","bodyOutline")
+         .attr("d", function(){
+             var pathStr = "M ";
+ 
+             for(var i = 0; i < data2.length; i++)
+             {
+                 if(i > 0)
+                     pathStr += "L " + data2[i].x * xscale + " " + data2[i].y * yscale+" ";
+                 else
+                     pathStr += data2[i].x * xscale + " " + data2[i].y * yscale +" ";
+                 if(i == data2.length-1)
+                     pathStr += "z";
+             }
+             return pathStr;
+         })
+         .style("fill", function(){return g_exprValColorMap(0.4);})
+         .style("opacity","0.5")
+          .attr("transform","translate("+ 70 +","+0+")");
+    });
+
+    // for(var s = 0; s < g_pathFiles.length; s++){
+    //     console.log(s);
+    //     d3.csv(g_pathFiles[s], function(data){
+    //         data.forEach(d => {
+    //             d.x = +d.x;
+    //             d.y = +d.y;
+    //         });
+    //         allPathData.push(data);
+    //         var xscale = 0.65*width/g_orgBodyImgWidth; // unclear why xscale is not correct???
+    //         var yscale = height/g_orgBodyImgHeight;
+    //         // set path
+
+    //         nodePartSilhouette.append('path')
+    //         .attr("class","bodyOutline")
+    //         .attr("d", function(){
+    //             var pathStr = "M ";
+
+    //             for(var i = 0; i < data.length; i++)
+    //             {
+    //                 if(i > 0)
+    //                     pathStr += "L " + data[i].x * xscale + " " + data[i].y * yscale+" ";
+    //                 else
+    //                     pathStr += data[i].x * xscale + " " + data[i].y * yscale +" ";
+    //                 if(i == data.length-1)
+    //                     pathStr += "z";
+    //             }
+    //             return pathStr;
+    //         })
+    //         .style("fill", function(d,i){return g_exprValColorMap(0.2 * s)})
+    //         .style("opacity","0.5")
+    //          .attr("transform","translate("+ 70 +","+0+")")
+    //         // .attr("transform","translate("+width/2+","+height/2+")");
+        
+    //     })
+
+    // }
+
+    svg_img.attr('height', height)
+        .attr('width', width)
+        .attr('xlink:href', logoUrl)
+        .attr("fill", "black")
+        .style('stroke', '#AAA')
+        .style("opacity",0.1)
+        ;
     return svg_img;
 }
 
@@ -66,6 +204,8 @@ function drawDotplots()
 
 function rnaViewerMain()
 {
+    // g_exprValColorMap = d3.scaleOrdinal(d3.schemeYlOrRd[5]);
+    g_exprValColorMap = d3.scaleSequential(d3.interpolateYlOrRd);
     // 0. set up views
     // drawBodyView(g_tmpMeanVal, "bodyMap", "#bodyView", g_bvwidth, g_bvheight, g_margin);
     // 0.load data
